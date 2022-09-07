1cfb451d4a0fad2e4d3dc28fded1db22dc92e01b
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-07 15:38:53 +0800

    Added axios, stabilize nullchecks, refactor

--- contents/cedula-mount.tsx
+++ contents/cedula-mount.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import iconImage from "data-base64:~assets/ph.png"
 import type { PlasmoContentScript } from "plasmo"
 import React from "react"
@@ -22,6 +23,63 @@ const ConstructImageElement = () => {
   return para.appendChild(img)
 }
 
+export const getMountPoint = async () => {
+  await AddCedulas()
+  return document.querySelector(all_tags.plasmo.feature_title)
+}
+
+const AddCedulas = async () => {
+  const imageElement = ConstructImageElement()
+  Object.values(all_tags.fb).forEach((tagList) => {
+    // const elements = TraceElements(tagList)
+    const elements = document.querySelectorAll(tagList.path[0])
+
+    for (const mountPoint of elements) {
+      console.log("mount", mountPoint)
+      const link = findLink(mountPoint)
+      console.log(link)
+
+      const toAppend = mountPoint.querySelector(":scope span")
+      if (toAppend && toAppend.getElementsByTagName("img").length == 0) {
+        console.log(toAppend.innerHTML)
+        const name = toAppend.innerHTML
+        toAppend.append(imageElement)
+        // toAppend.appendChild(imageElement)
+        // 1. Check the local storage/cache
+        // 2. If not found, make a request to the server
+        // 3. Store the response in the local storage/cache
+      }
+    }
+  })
+  return
+}
+
+const findLink = (element: Element): String => {
+  const firstChild = element.firstElementChild
+  if (firstChild) {
+    let link = firstChild.getAttribute("href")
+    if (!link) return null
+    link = link.substring(25, link.indexOf("?"))
+    return link
+  }
+  return null
+}
+
+// TODO: Switch to React Components when Issue #22 is resolved
+// REACT COMPONENT TO MOUNT
+const PlasmoPricingExtra = () => {
+  return (
+    <span
+      style={{
+        background: "white",
+        padding: 0,
+        blockSize: 0
+      }}></span>
+  )
+}
+
+export default PlasmoPricingExtra
+
 // TODO: Fix to dynamically trace queue from a given list
 function TraceElements(tagList) {
   let crawler
@@ -42,49 +100,3 @@ function TraceElements(tagList) {
   }
   return crawler
 }
-
-export const getMountPoint = async () => {
-  await AddCedulas()
-  return document.querySelector(all_tags.plasmo.feature_title)
-}
-
-const AddCedulas = async () => {
-  const imageElement = ConstructImageElement()
-  Object.values(all_tags.fb).forEach((tagList) => {
-    // const elements = TraceElements(tagList)
-    const elements = document.querySelectorAll(tagList.path[0])
-    // const textNode = document.createTextNode("Cedula")
-
-    for (const mountPoint of elements) {
-      const toAppend = mountPoint.querySelector(":scope span")
-      if (!toAppend) {
-        continue
-      }
-      // Use this to debug where the mount is being placed
-      // if (toAppend && !toAppend.lastChild?.textContent?.includes("Cedula")) {
-      //   toAppend.appendChild(textNode)
-      // }
-      //
-      if (toAppend && toAppend.getElementsByTagName("img").length == 0) {
-        toAppend.appendChild(imageElement)
-        console.log(toAppend.firstChild.textContent)
-      }
-    }
-  })
-}
-
-// TODO: Switch to React Components when Issue #22 is resolved
-// REACT COMPONENT TO MOUNT
-const PlasmoPricingExtra = () => {
-  return (
-    <span
-      style={{
-        background: "white",
-        padding: 0,
-        blockSize: 0
-      }}>
-    </span>
-  )
-}
-
-export default PlasmoPricingExtra
