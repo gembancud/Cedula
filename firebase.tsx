3dee04ea11027f9d35135d02f708c2a0425cb2ff
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-08 07:49:08 +0800

    Reverted env scheme to PLASMO_PUBLIC

--- firebase.tsx
+++ firebase.tsx
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.CEDULA_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.CEDULA_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.CEDULA_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.CEDULA_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.CEDULA_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.CEDULA_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.CEDULA_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.PLASMO_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.PLASMO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PLASMO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PLASMO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PLASMO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PLASMO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.PLASMO_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
