9f60c1c8f2e3c2cb534faf38d667db5b7c1cca59
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-08 23:33:49 +0800

    invoke AddCedulas in tabs.onUpdated

--- background.ts
+++ background.ts
@@ -1,13 +1,12 @@
+import { AddCedulas } from "./cedula"
+
 export {}
 
 // console.log("hello from background")
 //
 //
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log("update called")
   if (tab.url.includes("facebook.com")) {
-    if (changeInfo.status == "loading") {
-      console.log("loadcomplete")
-    }
+    AddCedulas()
   }
 })
