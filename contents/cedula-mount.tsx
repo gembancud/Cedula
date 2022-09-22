1c14fe438d885e4e88665fe79cdc5bf76f109dc3
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-23 02:59:30 +0800

    Added semaphore functionality to reduce scroll calls

--- contents/cedula-mount.tsx
+++ contents/cedula-mount.tsx
@@ -1,8 +1,7 @@
-import axios from "axios"
 import type { PlasmoContentScript } from "plasmo"
 import React from "react"
 
-import { AddCedulas, storage } from "../cedula"
+import { AddCedulas } from "../cedula"
 import { isMarked } from "../utils"
 
 export const config: PlasmoContentScript = {
@@ -13,12 +12,24 @@ export const config: PlasmoContentScript = {
 export const getMountPoint = async () => {
   if (!isMarked("cedula_marked", document.head))
     window.addEventListener("click", async () => {
-      // await strictSingleOp("semaphore", AddCedulas)
       await AddCedulas()
     })
-  // await strictSingleOp("semaphore", AddCedulas)
-  await AddCedulas({ markAll: true })
+  await strictSingleOp(AddCedulas)
+  // await AddCedulas()
   return document.querySelector("div")
+}
+
+// This is prone to breaking.
+// Call using strictSingleOp(AddCedulas)
+// No more no less
+let strictSingleOpSem = 0
+const strictSingleOp = async (callback) => {
+  if (strictSingleOpSem === 1) {
+    return
+  }
+  strictSingleOpSem = 1
+  await callback()
+  strictSingleOpSem = 0
 }
 
 // TODO: Switch to React Components when Issue #22 is resolved
@@ -33,39 +44,4 @@ const PlasmoPricingExtra = () => {
       }}></span>
   )
 }
-
-const strictSingleOp = async (val, callback) => {
-  const semaphore = await storage.get(val)
-  if (!semaphore || semaphore === "false") {
-    await storage.set(val, "true")
-  } else if (semaphore === "true") {
-    console.log("Supposed to stop here")
-    return
-  }
-  console.log("triggering callback", await storage.get(val))
-  await callback()
-  await storage.set(val, "false")
-}
-
 export default PlasmoPricingExtra
-
-// TODO: Fix to dynamically trace queue from a given list
-function TraceElements(tagList) {
-  let crawler
-  if (tagList.query[0] === 0) {
-    crawler = document.querySelectorAll(tagList.path[0])
-  } else {
-    crawler = document.querySelector(tagList.path[0])
-  }
-
-  for (let i = 1; i < tagList.path.length; i++) {
-    if (tagList.query[i] == 0) {
-      crawler = crawler.querySelectorAll(tagList.path[i])
-    } else {
-      if (crawler) {
-        crawler = crawler.querySelector(tagList.path[i])
-      }
-    }
-  }
-  return crawler
-}
