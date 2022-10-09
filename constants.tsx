c9499c4781db32ff2a72421904767508aa17bfcc
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-10-10 04:47:29 +0800

    Updated missing tags

--- constants.tsx
+++ constants.tsx
@@ -12,8 +12,11 @@ export const all_tags = {
     newsfeed_name:
       "span.rse6dlih:not(.rrjlc0n4):not(.jwegzro5) > span:not([cedula_marked]) > span:not([cedula_marked])",
     comment_name:
-      "span.fxk3tzhb:not(.p8bhzyuu):not(.ewjwymqb ):not(.b2rh1bv3):not(.c7y9u1f0) > span:not([cedula_marked])"
-
+      "span.fxk3tzhb:not(.p8bhzyuu):not(.ewjwymqb ):not(.b2rh1bv3):not(.c7y9u1f0) > span:not([cedula_marked])",
+    is_with_post:
+      "span.x193iq5w > strong > span > a.x1i10hfl > span.xt0psk2:not([cedula_marked]) > span:not([cedula_marked])",
+    comment_name_bold:
+      "span.xt0psk2 > a.x1i10hfl > span.x3nfvp2 > span.x193iq5w:not([cedula_marked])"
     // profile_name: {
     //   path: ["h1.jxuftiz4.jwegzro5.hl4rid49.icdlwmnq"],
     //   query: [1]
